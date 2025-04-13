7b7b6e82289f32d5d2e21b5259358312ef2c8fa0
samuelj1323 <samuelj1323@gmail.com>
2025-04-12 22:57:15 -0400

    Implement authentication middleware and enhance todo routes with CRUD operations

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -39,8 +39,30 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
   // Compare the two encrypted passwords
-  console.log(`usrname: ${username}, pwd: ${password}`);
-  res.sendStatus(200);
+  try {
+    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
+    const user = getUser.get(username);
+
+    if (!user) {
+      return res.status(404).send({ message: "user not found" });
+    }
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "invalid password" });
+    }
+    // Success
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+    res.json({ token });
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(503);
+  }
 });
 
 export default router;
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -3,13 +3,41 @@ import db from "../db.js";
 
 const router = express.Router();
 
-router.get("/", (req, resp) => {});
+router.get("/", (req, resp) => {
+  const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`);
+  const todos = getTodos.all(req.userId);
+  resp.status(200).json(todos);
+});
 
 // Create a new todo
-router.post("/", (req, resp) => {});
+router.post("/", (req, resp) => {
+  const { task } = req.body;
+  const insertTodo = db.prepare(
+    `INSERT INTO todos (user_id, task) VALUES(?, ?)`
+  );
+  const result = insertTodo.run(req.userId, task);
 
-router.put("/:id", (req, resp) => {});
+  resp.json({ id: result.lastInsertRowid, task, completed: 0 });
+});
 
-router.delete("/:id", (req, resp) => {});
+router.put("/:id", (req, resp) => {
+  const { completed } = req.body;
+  const { id } = req.params;
+  const { page } = req.query;
+
+  const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`);
+  updatedTodo.run(completed, id);
+  resp.json({ message: "Todo completed" });
+});
+
+router.delete("/:id", (req, resp) => {
+  const { id } = req.params;
+  const userId = req.userId;
+  const deleteTodo = db.prepare(
+    `DELETE FROM todos WHERE id = ? AND user_id = ?`
+  );
+  deleteTodo.run(id, userId);
+  resp.json({ message: "Todo deleted" });
+});
 
 export default router;
